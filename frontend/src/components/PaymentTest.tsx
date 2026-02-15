b64c984d5c9edd0b32894b7f4db161c20144a7bc
hoshoria <hoshoria@hotmail.com>
2026-02-15 15:06:38 -0500

    Migrate to NestJS + React, remove lockscreen, fix menu lag, improve responsive design

--- frontend/src/components/PaymentTest.tsx
+++ frontend/src/components/PaymentTest.tsx
@@ -1,10 +1,6 @@
 import '../styles/PaymentTest.css';
 
 const LINKS = [
-    { icon: 'fas fa-shopping-bag', label: 'Motive Products', url: 'https://www.motiveproducts.com/checkouts/cn/hWN85uFhIJ2ZXHhJr3pYi2Hy/en-us/information?_r=AQABZOt9S9UpjW_oE8SOfByre1VN--h4Nlih20wjNhLfNYY&skip_shop_pay=true' },
-    { icon: 'fas fa-tshirt', label: 'Desert Noir', url: 'https://desert-noir.myshopify.com/checkouts/cn/hWN85uJ0YuoTuev6j19fpSra/en-pe?_r=AQABA49A2bMja4vDK0B33sfG2vgpZimegDVSctXcQjQpLTY&skip_shop_pay=true' },
-    { icon: 'fas fa-tools', label: 'Demon Workshop', url: 'https://demon-workshop.com/checkouts/cn/hWN85uMFy1EvTsWF1NNKtp9W/en-us?_r=AQABkliPA8vPtjpbb-ZN6yslvTy36aXhABnz4qPr6TgbAKg&auto_redirect=false&edge_redirect=true&skip_shop_pay=true' },
-    { icon: 'fas fa-graduation-cap', label: 'W3Schools', url: 'https://campus.w3schools.com/checkouts/cn/hWN85uOVcApKui2vr1A7xaks/en-pe?_r=AQAB7waapMgozdIPiDVuIjy-8smUAy4smdkP4bQl67fBpYI&skip_shop_pay=true' },
     { icon: 'fas fa-shopping-cart', label: 'Yona West', url: 'https://www.yonawest.com/checkouts/cn/hWN8g8g2TXzetehrdUNDRZQI/es-us?_r=AQABFi4mwoao81Ks7DbGXx6Xl-tItDBtH4mTEpJjRhoF7Wo&auto_redirect=false&edge_redirect=true&skip_shop_pay=true' },
 ];
 
